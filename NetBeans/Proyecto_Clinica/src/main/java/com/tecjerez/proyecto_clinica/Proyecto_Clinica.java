f9ff7b0a0b11f689fbd1d22f6c5e0770c83b37c8
Eduardo Alejo <eduardo.br@tecjerez.edu.mx>
2024-11-19 17:41:36 -0600

    Primer commit, inicializacion proyecto en NetBeans

---
+++ NetBeans/Proyecto_Clinica/src/main/java/com/tecjerez/proyecto_clinica/Proyecto_Clinica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tecjerez.proyecto_clinica;
+
+/**
+ *
+ * @author ed308
+ */
+public class Proyecto_Clinica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
